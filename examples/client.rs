edd4d71e12542e27e0c3b0dd88a351ea8ae0d5f6
AidoP <aidan@trifuse.xyz>
2022-01-01 14:50:22 +0800

    Use new Message creation functionality in custom client example

--- examples/client.rs
+++ examples/client.rs
@@ -3,17 +3,9 @@ use std::{os::unix::{net::*, prelude::AsRawFd}, io::IoSlice, fs::File};
 
 fn main() {
     let file = File::open("readme.md").unwrap();
-    let socket = UnixStream::connect("/run/user/1000/wayland-0")
+    let mut stream = UnixStream::connect("/run/user/1000/wayland-0")
         .unwrap();
-
-    let mut buf = [0; 128];
-    let mut anc = SocketAncillary::new(&mut buf);
-    anc.add_fds(&[file.as_raw_fd()]);
-
-    let buf = [01i32, 0x00080000];
-    let buf: [u8; 2 * 4] = unsafe { std::mem::transmute(buf) };
-    socket.send_vectored_with_ancillary(
-        &[IoSlice::new(&buf)], 
-        &mut anc
-    ).unwrap();
+    let mut message = wl::Message::new(1, 0);
+    message.push_fd(file.as_raw_fd());
+    message.send(&mut stream).unwrap();
 }
